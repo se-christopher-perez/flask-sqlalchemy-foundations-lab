a3cf015fcfb5a111f03bfb0dfac5d94eeb99c0c1
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-18 19:48:25 -0400

    update

--- server/app.py
+++ server/app.py
@@ -22,6 +22,40 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    found = Earthquake.query.filter_by(id=id).first()
+
+    if found:
+        return make_response({
+            "id": found.id,
+            "location": found.location,
+            "magnitude": found.magnitude,
+            "year": found.year
+        }, 200)
+    else:
+        return make_response(
+            {"message": f"Earthquake {id} not found."}, 404
+        )
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    found = Earthquake.query.filter(
+        Earthquake.magnitude >= magnitude
+    ).all()
+
+    return make_response({
+        "count": len(found),
+        "quakes": [
+            {
+                "id": quake.id,
+                "location": quake.location,
+                "magnitude": quake.magnitude,
+                "year": quake.year
+            }
+            for quake in found
+        ]}, 200)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
